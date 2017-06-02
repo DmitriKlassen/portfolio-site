6d8f3543aad941493a0cacdd85789428fdfe381a
Dmitri Laptop <Dmi.Klassen@gmail.com>
2017-06-02 16:59:36 -0400

    icon gallery styling

--- js/script.js
+++ js/script.js
@@ -9,4 +9,17 @@ $(function () {
     $("#banner svg path:nth-child(" + String(i + 1) + ")")
       .css("animation-delay", String((i) * .15) + "s");
   }
+
+  // handle changing skills content
+  $(".skill-gallery .icon-container").click(function() {
+    $(".skill-gallery .icon-container").addClass("hidden");
+    $(".head-skills .icon-container:nth-child(" + String($(this).index() + 1) + ")").addClass("visible");
+    $(".skill-description .description-container:nth-child(" + String($(this).index() + 1) + ")").addClass("visible");
+  });
+  
+  $(".head-skills .icon-container").click(function() {
+    $(".skill-gallery .icon-container").removeClass("hidden");
+    $(".head-skills .icon-container").removeClass("visible");
+    $(".skill-description .description-container").removeClass("visible");
+  });
 });
